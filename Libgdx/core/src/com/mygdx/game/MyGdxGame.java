2ba4061c1b85a3ef3080540aa4c7827ed778bf1a
Paul Cupido <pcupido@effcomm.com>
2016-02-14 10:49:54 +0200

    Added a sprite version of hello world with rotations and scaling

--- Libgdx/core/src/com/mygdx/game/MyGdxGame.java
+++ Libgdx/core/src/com/mygdx/game/MyGdxGame.java
@@ -4,7 +4,9 @@ import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 
@@ -12,16 +14,34 @@ public class MyGdxGame implements ApplicationListener
 {
 	private SpriteBatch batch;
 	private BitmapFont font;
+	private Texture img;
+	private Sprite sprite;
 
 	/** 
 	 * The create method sees to it that all instance variables are initialized
 	 * */
 	public void create() 
 	{
+		// instantiates the sprite batch
 		batch = new SpriteBatch();
 		font = new BitmapFont();
 		font.setColor(Color.RED);
 		
+		// instantiates the texture
+		img = new Texture("hello-world.png");
+		
+		// instantiates the sprite
+		sprite = new Sprite(img);
+		
+		// sets the position of the sprite on the screen - sprite will be drawn in the center
+		sprite.setPosition(Gdx.graphics.getWidth()/2 - sprite.getWidth()/2, 
+				Gdx.graphics.getHeight()/2 - sprite.getHeight()/2);
+		
+		// rotates the sprite 90 degrees (pi/2 radians) about the origin (which is set to 0,0 by default)
+		sprite.setRotation(90f);
+		
+		// scales the sprite
+		sprite.setScale(2.0f, 1.0f);
 	}
 
 	@Override
@@ -37,14 +57,26 @@ public class MyGdxGame implements ApplicationListener
 		// open up open gl inside here and draw all objects which are to be drawn
 		// no calculations related to game artifacts should be done here - and not while open gl is drawing
 			
-		Gdx.gl.glClearColor(1, 1, 1, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		
 		batch.begin();
 		// draw textures/ artifacts here
-		// need to find out how to get the screen size - I want to centre this text and then have it print out the diary of prophecy
-		// Come my people and shut thy doors about thee, for a great power has come to punish the weak for their inequity and it shall be the end of the world as you know it.
+		
+		// Challenge 1: Print out the diary of prophecy
+		// Challenge 2: Create the Star Wars crawl
+		
 		font.draw(batch, "Hello World", 400, 400);
+		
+		// draws the sprite - here I'm rotating the sprite
+		batch.draw(sprite, sprite.getX(), sprite.getY(), 
+				sprite.getWidth()/2, 
+				sprite.getHeight()/2, 
+				sprite.getWidth(),
+				sprite.getHeight(),
+				sprite.getScaleX(), 
+				sprite.getScaleY(), 
+				sprite.getRotation());
 		batch.end();
 	
 	}
