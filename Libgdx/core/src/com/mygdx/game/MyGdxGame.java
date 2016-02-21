63ce628270402c6453f1cf8e26be3efab9e7f94d
Paul Cupido <pcupido@effcomm.com>
2016-02-21 12:04:23 +0200

    Completed the introduction to srptesheets and texture atlases tutorial. Updated the readme.

--- Libgdx/core/src/com/mygdx/game/MyGdxGame.java
+++ Libgdx/core/src/com/mygdx/game/MyGdxGame.java
@@ -1,107 +1,134 @@
 package com.mygdx.game;
 
-import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 
-public class MyGdxGame implements ApplicationListener
+public class MyGdxGame extends ApplicationAdapter implements InputProcessor
 {
-	private SpriteBatch batch;
-	private BitmapFont font;
-	private Texture img;
+	// public variables
+	public static final int MAX_FRAMES = 19;
+	
+	// private variables
 	private Sprite sprite;
-
-	/** 
-	 * The create method sees to it that all instance variables are initialized
-	 * */
+	private SpriteBatch batch;
+	private TextureAtlas atlas;
+	private TextureRegion region;
+	
+	private int currentFrame =1;
+	
 	public void create() 
 	{
 		// instantiates the sprite batch
 		batch = new SpriteBatch();
-		font = new BitmapFont();
-		font.setColor(Color.RED);
 		
 		// instantiates the texture
-		img = new Texture("hello-world.png");
+		atlas = new TextureAtlas(Gdx.files.internal("SpriteSheets/spritesheet.atlas"));
 		
-		// instantiates the sprite
-		sprite = new Sprite(img);
+		// instantiate the regions by manually finding one from the atlas
+		region = atlas.findRegion("0001");
+		
+		// instantiates the sprite from the region
+		sprite = new Sprite(region);
 		
 		// sets the position of the sprite on the screen - sprite will be drawn in the center
 		sprite.setPosition(Gdx.graphics.getWidth()/2 - sprite.getWidth()/2, 
 				Gdx.graphics.getHeight()/2 - sprite.getHeight()/2);
 		
-		// rotates the sprite 90 degrees (pi/2 radians) about the origin (which is set to 0,0 by default)
-		sprite.setRotation(90f);
-		
-		// scales the sprite
-		sprite.setScale(2.0f, 1.0f);
-	}
-
-	@Override
-	public void resize(int width, int height) 
-	{
-		// TODO Auto-generated method stub
-		
+		// registers the the input processor as the one implemented by this class
+		Gdx.input.setInputProcessor(this);
 	}
 
 	/** Renders the scene*/
 	public void render() 
 	{
-		// open up open gl inside here and draw all objects which are to be drawn
-		// no calculations related to game artifacts should be done here - and not while open gl is drawing
 			
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		
 		batch.begin();
-		// draw textures/ artifacts here
-		
-		// Challenge 1: Print out the diary of prophecy
-		// Challenge 2: Create the Star Wars crawl
-		
-		font.draw(batch, "Hello World", 400, 400);
-		
-		// draws the sprite - here I'm rotating the sprite
-		batch.draw(sprite, sprite.getX(), sprite.getY(), 
-				sprite.getWidth()/2, 
-				sprite.getHeight()/2, 
-				sprite.getWidth(),
-				sprite.getHeight(),
-				sprite.getScaleX(), 
-				sprite.getScaleY(), 
-				sprite.getRotation());
+		sprite.draw(batch);
 		batch.end();
 	
 	}
 
-	@Override
-	public void pause() 
-	{
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void resume() 
-	{
-		// TODO Auto-generated method stub
-		
-	}
-
-	/** Destroys unnecessary / no longer used objects - functionally similar to a destructor method, I imagine?*/
 	public void dispose() 
 	{
-		// free up the resources held by font and batch
-		font.dispose();
+		atlas.dispose();
 		batch.dispose();
+	}
+
+	/** Activates when the key is pressed*/
+	public boolean keyDown(int keycode) 
+	{
+		// Cycles through the sprite sheet each time a key is pressed up or down and updates the sprite
+		// with that information
 		
+		if (keycode == Input.Keys.UP) {
+			currentFrame++;
+			if (currentFrame > MAX_FRAMES) {
+				currentFrame = 1;
+			}
+		}
+		if (keycode == Input.Keys.DOWN) {
+			currentFrame--;
+			if (currentFrame < 1) {
+				currentFrame = MAX_FRAMES;
+			}
+		}
+		
+		sprite.setRegion(atlas.findRegion(String.format("%04d", currentFrame)));
+		
+		return false;
+	}
+
+	/** Activates when the key is released*/
+	public boolean keyUp(int keycode) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(char character) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(int amount) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 	
 }
--- Readme.txt
+++ Readme.txt
@@ -1,7 +1,2 @@
 My repo to hold the code that I write as I work through lib-gdx tutorials
-- This branch will largely stay empty while the sub-branches contain the relevant code
-
-The repository contains two types of branches: tutorials and challenges
-
-	Tutorials: These contain code written during/ after going through a tutorial on a specific aspect of lib-gdx.
-	Challenges: These contain the code for challenges that I set for myself to apply the work that I learn in the tutorial .
+Current tutorial: introduction to spritesheets and texture atlas
