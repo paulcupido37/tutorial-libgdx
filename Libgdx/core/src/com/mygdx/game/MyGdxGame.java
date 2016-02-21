e313d28ee949fb73e7c97486c17d1d86630f994f
Paul Cupido <pcupido@effcomm.com>
2016-02-21 17:26:42 +0200

    Created the inital menu system - will want to apply user input and sprite animations in the next commit

--- Libgdx/core/src/com/mygdx/game/MyGdxGame.java
+++ Libgdx/core/src/com/mygdx/game/MyGdxGame.java
@@ -2,6 +2,8 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
@@ -12,41 +14,69 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.utils.Array;
 
 
-public class MyGdxGame extends ApplicationAdapter
+public class MyGdxGame extends ApplicationAdapter implements InputProcessor
 {
 	private TextureAtlas textureAtlas;
 	private TextureRegion textureRegion;
 	
 	private SpriteBatch batch;
 	private Sprite sprite;
+	
+	private Sprite[] allSprites;
+	
+	private int selectButton =0;
 
 	/** 
 	 * The create method sees to it that all instance variables are initialized
 	 * */
 	public void create() 
 	{
+		
+		String[] text = {"campaign-button", 
+						 "challenge-button", 
+						 "shop-button", 
+						 "quit-button", 
+						 "legends-banner", 
+						 "legends-heroes"};
+		
 		// instantiates the sprite batch
 		batch = new SpriteBatch();
 		
 		// instantiates the texture atlas and region
 		textureAtlas = new TextureAtlas(Gdx.files.internal("spritesheets/mainmenu-spritesheet.atlas"));
 		textureRegion = textureAtlas.findRegion("legends-banner");
+		allSprites = new Sprite[6];
 		
-		/* It may be better to use an array of texture regions.
-		 * The other option is to use the textureAtlas.findRegion() method to select the texture to be displayed.
-		 * This would then require an array of sprites, though I think those are necessary anyway. */
+		// Initialize the sprites with specific texture regions provided by the text
+		for (int i = 0; i < allSprites.length && i < text.length; i ++ )
+		{
+			allSprites[i] = new Sprite(textureAtlas.findRegion(text[i]));
+			allSprites[i].setRegion(textureAtlas.findRegion(text[i]));
+		}
 		
-		Array<AtlasRegion> regions = textureAtlas.getRegions();
+		// Positional data - must find a better way to deal with this
+		int width = Gdx.graphics.getWidth();
+		int height = Gdx.graphics.getHeight();
 		
-		// need to check if this the best way to do the sprite switching
-		sprite = new Sprite(new Texture(Gdx.files.internal("spritesheets/mainmenu-spritesheet.png")));
-		//sprite = textureRegion.;//setRegion(textureAtlas.findRegion("legends-banner").getTexture());
+		allSprites[0].setPosition(allSprites[2].getWidth()/2, height/2 - (height/12));
 		
-		// sets the position of the sprite on the screen - sprite will be drawn in the center
-		sprite.setPosition(Gdx.graphics.getWidth()/2 - sprite.getWidth()/2, 
-				Gdx.graphics.getHeight()/2 - sprite.getHeight()/2);
+		allSprites[1].setPosition(allSprites[2].getWidth()/2,
+				height/2 - 1.5f * allSprites[2].getHeight() - (height/12));
 		
+		allSprites[2].setPosition(allSprites[2].getWidth()/2,
+				height/2 - 3 * allSprites[2].getHeight() - (height/12));
 		
+		allSprites[3].setPosition(width - ((1.5f) * allSprites[3].getWidth()),
+				height/2 - 3 * allSprites[2].getHeight() - (height/12));
+		
+		allSprites[4].setPosition(width/2 - allSprites[4].getWidth()/2,
+				height - allSprites[4].getHeight());
+		
+		allSprites[5].setPosition(width - (1.125f * allSprites[5].getWidth()),
+				(0.75f) * allSprites[5].getHeight());
+		
+		//Set the input
+		Gdx.input.setInputProcessor(this);
 	}
 
 	@Override
@@ -59,16 +89,18 @@ public class MyGdxGame extends ApplicationAdapter
 	/** Renders the scene*/
 	public void render() 
 	{
-		// open up open gl inside here and draw all objects which are to be drawn
-		// no calculations related to game artifacts should be done here - and not while open gl is drawing
-			
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		
 		batch.begin();
 		
 		// draws the sprite - here I'm rotating the sprite
-		sprite.draw(batch);
+		allSprites[0].draw(batch);
+		allSprites[1].draw(batch);
+		allSprites[2].draw(batch);
+		allSprites[3].draw(batch);
+		allSprites[4].draw(batch);
+		allSprites[5].draw(batch);
 		batch.end();
 	
 	}
@@ -78,6 +110,58 @@ public class MyGdxGame extends ApplicationAdapter
 		// free up the resources held by font and batch
 		batch.dispose();
 		
+	}
+
+	@Override
+	public boolean keyDown(int keycode) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean keyUp(int keycode) {
+		// TODO Auto-generated method stub
+		if (keycode == Input.Keys.UP) {
+			// set selected sprite
+		}
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(char character) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
+		// want to set a button as selected when the mouse moves over it
+	
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(int amount) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 	
 }
