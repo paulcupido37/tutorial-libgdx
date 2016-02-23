30afd69abadf60b60a926726654f3708c5868fc9
Paul Cupido <pcupido@effcomm.com>
2016-02-23 11:51:05 +0200

    Completed the simple animation tutorial

--- Libgdx/core/src/com/mygdx/game/MyGdxGame.java
+++ Libgdx/core/src/com/mygdx/game/MyGdxGame.java
@@ -2,23 +2,24 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 
 public class MyGdxGame implements ApplicationListener
 {
 	private SpriteBatch batch;
-	private BitmapFont font;
+	private Animation animation;
+	private TextureAtlas textureAtlas;
 
+	private float elapsedTime = 0;
+	
 	public void create() 
 	{
-		// instantiates the sprite batch
 		batch = new SpriteBatch();
-		font = new BitmapFont();
-		font.setColor(Color.RED);
+		textureAtlas = new TextureAtlas(Gdx.files.internal("spritesheet.atlas"));
+		animation = new Animation(1/30f, textureAtlas.getRegions());
 	}
 
 	@Override
@@ -30,11 +31,10 @@ public class MyGdxGame implements ApplicationListener
 	{
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		elapsedTime += Gdx.graphics.getDeltaTime();
 		
 		batch.begin();
-		font.draw(batch, "Hello World", 
-				Gdx.graphics.getWidth()/2 - font.getLineHeight()/2, 
-				Gdx.graphics.getHeight()/2 - font.getLineHeight()/2);
+		batch.draw(animation.getKeyFrame(elapsedTime, true), 0, 0);
 		batch.end();
 	}
 
@@ -50,8 +50,8 @@ public class MyGdxGame implements ApplicationListener
 
 	public void dispose() 
 	{
-		font.dispose();
-		batch.dispose();		
+		batch.dispose();
+		textureAtlas.dispose();
 	}
 	
 }
--- Readme.txt
+++ Readme.txt
@@ -1,2 +1,2 @@
 My repo to hold the code that I write as I work through lib-gdx tutorials
-This branch will serve as the basis for all other branches
+Current tutorial: Simple animation using the animation class
