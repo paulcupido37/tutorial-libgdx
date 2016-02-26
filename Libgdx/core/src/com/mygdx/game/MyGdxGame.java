51ca16d8c622f509fc032b4324ecb35bf610b858
Paul Cupido <pcupido@effcomm.com>
2016-02-26 21:01:54 +0200

    Added comments to the code

--- Libgdx/core/src/com/mygdx/game/MyGdxGame.java
+++ Libgdx/core/src/com/mygdx/game/MyGdxGame.java
@@ -1,5 +1,6 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
@@ -11,34 +12,47 @@ import com.badlogic.gdx.graphics.Texture.TextureFilter;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class MyGdxGame implements ApplicationListener, InputProcessor
+/** Class to model a basic orthographic camera and its movement around the screen
+ * 
+ * @author Paul Cupido
+ * @date 26 February 2016
+ * */
+
+public class MyGdxGame extends ApplicationAdapter implements InputProcessor
 {
+	// Sprite and SpriteBatch
+	private Sprite sprite;
+	private SpriteBatch batch;
+	
+	// Texture and Orthographic Camera
+	private Texture texture;
 	private OrthographicCamera camera;
 	
-	private SpriteBatch batch;
-	private Texture texture;
-	private Sprite sprite;
-	
-	private boolean cameraMovement = false;
-	
-	private int numberOfInputs = 0; 
+	// Translation coordinates 
 	private float translateX = 0f;
 	private float translateY = 0f;
 	
+	// Counter for the number of inputs
+	private int numberOfInputs = 0;
+	
+	// Boolean to determine whether or not the camera is active 
+	private boolean cameraMovement = false;
+	
 	/**
+	 * Initializes the camera, texture and sprite
+	 * 
 	 * @todo Add a viewport to stop the camera from going passed the bounds of the image
 	 * @todo Create a separate class for input processing - extend Input Adapter
-	 * @todo Comment this file properly
 	 * */
 	public void create() 
 	{
-		// With a 2d game - Orthogonal camera
+		// Initialize the Orthographic Camera at 1280 x 720p
 		camera = new OrthographicCamera(1280, 720);
 		
 		// instantiates the sprite batch
 		batch = new SpriteBatch();
 		
-		//load the texture
+		//loads the texture
 		texture = new Texture(Gdx.files.internal("Toronto Skyline.jpg"));
 		texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
 		
@@ -48,101 +62,100 @@ public class MyGdxGame implements ApplicationListener, InputProcessor
 		
 		Gdx.input.setInputProcessor(this);
 	}
-
+	
 	@Override
-	public void resize(int width, int height) 
-	{
-	}
-
 	public void render() 
 	{
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		
+		// Translates the camera and updates it depending on whether cameraMovement is true
 		if (cameraMovement) {
 			camera.translate(translateX, translateY);
 			camera.update();
 		}
 		
+		// Ties the LibGDX camera to the OpenGL renderer
 		batch.setProjectionMatrix(camera.combined);
+		
+		// Draw the sprite
 		batch.begin();
-		
-		sprite.draw(batch);
-		
+		sprite.draw(batch);	
 		batch.end();
 	}
 
 	@Override
-	public void pause() 
-	{		
-	}
-
-	@Override
-	public void resume() 
-	{		
-	}
-
 	public void dispose() 
 	{
 		batch.dispose();
 		texture.dispose();
 	}
 
-	@Override
-	public boolean keyDown(int keycode) {
-		
-		// inputs - counter to keep track of the number of keys that have been pressed
+	/** 
+	 * Starts moving the camera in a specific direction, depending on which of the directional keys has been pressed
+	 * */
+	public boolean keyDown(int keycode) 
+	{
+		// Increment a counter to keeps track of the number of keys that have been pressed
 		numberOfInputs++;
 		
-		// sets the direction of translation based on the key that has been pressed
+		// Sets the direction of translation based on the key that has been pressed
 		switch (keycode) {
 		
-		case(Input.Keys.RIGHT): 
-			
-			translateX = 1.0f;
-			cameraMovement = true;
-			break;
-		case(Input.Keys.UP): 
-			translateY = 1.0f;
-			cameraMovement = true;
-			break;
-		case(Input.Keys.DOWN): 
-			translateY = -1.0f;
-			cameraMovement = true;
-			break;
-		case(Input.Keys.LEFT): 
-			translateX = -1.0f;
-			cameraMovement = true;
-			break;
+			case(Input.Keys.RIGHT): 
+				
+				translateX = 1.0f;
+				cameraMovement = true;
+				break;
+				
+			case(Input.Keys.UP): 
+				translateY = 1.0f;
+				cameraMovement = true;
+				break;
+				
+			case(Input.Keys.DOWN): 
+				translateY = -1.0f;
+				cameraMovement = true;
+				break;
+				
+			case(Input.Keys.LEFT): 
+				translateX = -1.0f;
+				cameraMovement = true;
+				break;
 	}
 		
 		return false;
 	}
 
-	@Override
-	public boolean keyUp(int keycode) {
-		// Stop moving the camera when the user releases the keys
-		
-		// decrement counter
+	/**
+	 * Stops the camera from moving when the user releases a relevant key
+	 * */
+	public boolean keyUp(int keycode) 
+	{
+		// Decrement the counter that keeps track of the number of keys that have been pressed
 		numberOfInputs--;
 		
+		// Sets the camera translation on a specific axis to 0 if the relevant key is released
 		switch (keycode) {
 			
 			case(Input.Keys.RIGHT): 
 				translateX = 0f;
 				break;
+				
 			case(Input.Keys.UP): 
 				translateY = 0f;
 				break;
+				
 			case(Input.Keys.DOWN): 
 				translateY = 0f;
 				break;
+				
 			case(Input.Keys.LEFT): 
 				translateX = 0f;
 				break;
 		}
 		
-		// if there no keys are being pressed, then the camera stops moving
+		// If there no keys are being pressed, then the camera stops moving
 		if (numberOfInputs == 0) {
 			cameraMovement = false;
 		}
