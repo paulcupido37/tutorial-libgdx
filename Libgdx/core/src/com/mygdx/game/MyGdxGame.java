9b276560bc9b83aeef6a63383363bd5664b5dc10
Paul Cupido <pcupido@effcomm.com>
2016-02-27 10:06:10 +0200

    Started the tutorial

--- Libgdx/core/src/com/mygdx/game/MyGdxGame.java
+++ Libgdx/core/src/com/mygdx/game/MyGdxGame.java
@@ -1,29 +1,24 @@
 package com.mygdx.game;
 
-import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Color;
+
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.Texture;
 
-
-public class MyGdxGame implements ApplicationListener
+public class MyGdxGame extends ApplicationAdapter
 {
-	private SpriteBatch batch;
-	private BitmapFont font;
 
+	private Sprite sprite;
+	private Texture texture;
+	private SpriteBatch batch;
+	
 	public void create() 
 	{
-		// instantiates the sprite batch
+		
 		batch = new SpriteBatch();
-		font = new BitmapFont();
-		font.setColor(Color.RED);
-	}
-
-	@Override
-	public void resize(int width, int height) 
-	{
 	}
 
 	public void render() 
@@ -32,26 +27,13 @@ public class MyGdxGame implements ApplicationListener
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		
 		batch.begin();
-		font.draw(batch, "Hello World", 
-				Gdx.graphics.getWidth()/2 - font.getLineHeight()/2, 
-				Gdx.graphics.getHeight()/2 - font.getLineHeight()/2);
 		batch.end();
-	}
-
-	@Override
-	public void pause() 
-	{		
-	}
-
-	@Override
-	public void resume() 
-	{		
 	}
 
 	public void dispose() 
 	{
-		font.dispose();
-		batch.dispose();		
+		batch.dispose();
+		texture.dispose();
 	}
 	
 }
