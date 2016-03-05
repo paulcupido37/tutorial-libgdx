baf96e1f5136857a1d999800373bc5f5e70710eb
Paul Cupido <pcupido@effcomm.com>
2016-03-06 00:24:44 +0200

    Completed the basic audio tutorial for Lib-Gdx"

--- Libgdx/core/src/com/mygdx/game/MyGdxGame.java
+++ Libgdx/core/src/com/mygdx/game/MyGdxGame.java
@@ -2,10 +2,14 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.Timer;
+import com.badlogic.gdx.utils.Timer.Task;
 
 
 public class MyGdxGame implements ApplicationListener
@@ -13,12 +17,40 @@ public class MyGdxGame implements ApplicationListener
 	private SpriteBatch batch;
 	private BitmapFont font;
 
+	private Music music;
+	private long musicId;
+	
 	public void create() 
 	{
 		// instantiates the sprite batch
 		batch = new SpriteBatch();
 		font = new BitmapFont();
 		font.setColor(Color.RED);
+		
+		// Music class is used to stream larger sounds such as tracks
+		music = Gdx.audio.newMusic(Gdx.files.internal("easy.mp3"));
+		music.play();
+		
+		// set the sound to loop when it finishes
+		music.setLooping(true);
+		
+		// pan only works if the audio has been recorded in mono
+		// music.setPan(1.0f, 1.0f);
+		// Decreases the volume every 0.5 seconds and stops the song once the volume is less than 10%
+		Timer.schedule(new Task() {
+			public void run() {
+				
+				if (music.getVolume() <= 0.10f) {
+					// stops the music
+					music.stop();
+					
+					// cancels the task once the music dips below 10% volume
+					this.cancel();
+				} else {
+					music.setVolume(music.getVolume() - 0.1f);
+				}
+			}
+		}, 0.0f, 1f);
 	}
 
 	@Override
@@ -51,7 +83,8 @@ public class MyGdxGame implements ApplicationListener
 	public void dispose() 
 	{
 		font.dispose();
-		batch.dispose();		
+		batch.dispose();
+		music.dispose();
 	}
 	
 }
--- Readme.txt
+++ Readme.txt
@@ -1,5 +1,7 @@
 My repo to hold the code that I write as I work through lib-gdx tutorials
-Current Branch: Audio
+Current Branch: Audio Basics
 
 *in-progress*
-This branch will contain the audio/sound-effects tutorial for lib-gdx
+This branch contains the code for a basic audio/sound-effects tutorial in lib-gdx
+Specifically the branch contains a mp3 file having its volume lowered over time before being stopped entirely anf the timer
+being cancelled.
